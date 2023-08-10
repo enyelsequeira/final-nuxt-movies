da01ecb8ca468ff2f4c65f876ae4453166dde04e
EnyelSequeira <enyelsequeira@hotmail.com>
2023-08-10 18:36:00 +0100

    test21

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,18 @@ export default defineNuxtConfig({
     origin: process.env.ORIGIN as string,
     enableGlobalAppMiddleware: true,
   },
+  runtimeConfig: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    ORIGIN: process.env.ORIGIN,
+    public: {
+      API_KEY: process.env.API_KEY,
+    },
+  },
 });
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,7 +5,6 @@ import GithubProvider from "next-auth/providers/github";
 import { NuxtAuthHandler } from "#auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-import { env } from "~/env/server.mjs";
 
 export default NuxtAuthHandler({
   adapter: PrismaAdapter(prisma),
@@ -18,8 +17,8 @@ export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
+      clientId: useRuntimeConfig().GOOGLE_CLIENT_ID,
+      clientSecret: useRuntimeConfig().GOOGLE_CLIENT_SECRET,
     }),
     // GoogleProvider({
     //   clientId: env.GOOGLE_CLIENT_ID,
@@ -29,8 +28,6 @@ export default NuxtAuthHandler({
   callbacks: {
     // @ts-expect-error
     async session({ session, user }) {
-      // console.log("session callback");
-
       if (session.user) {
         session.user.id = user.id;
         session.user.email = user.email;

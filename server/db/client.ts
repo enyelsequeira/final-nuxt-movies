bf527366f8c3bedb8132c43ccee48814c55574c8
EnyelSequeira <enyelsequeira@hotmail.com>
2023-08-10 18:43:27 +0100

    test-3

--- server/db/client.ts
+++ server/db/client.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env/server.mjs";
 
 declare global {
   // eslint-disable-next-line no-var
@@ -10,9 +9,11 @@ export const prisma =
   global.prisma ||
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-if (env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
